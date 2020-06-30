b44ee7a49ce9c4e0539d4dd1a8fd10057582bbb2
Komal <consultant3@shopmonk.com>
2020-06-30 10:54:25 +0530

    WIP

---
+++ app/Http/Controllers/Api/CiscoRouterController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+ 
+use App\User;
+use Validator;
+use App\Router;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller; 
+ 
+class CiscoRouterController extends Controller
+ 
+{
+ 
+   public $successStatus = 200;
+ 
+   /**
+    * new router api - unique loopback and hostname values
+    *
+    * @return \Illuminate\Http\Response
+    */
+ 
+   public function create(Request $request)
+   {
+ 
+       $validator = Validator::make($request->all(), [
+ 
+            'sap_id'    => 'required', 
+            'hostname'  => 'required',
+            'type'      => 'required',
+            'loopback'  => 'required',
+            'mac_address' => 'required',
+ 
+       ]);
+//   return ($request->all());
+       if ($validator->fails()) {
+ 
+           return response()->json(['error'=>$validator->errors()], 401);            
+ 
+       }
+ 
+       $data    = $request->all();
+       $data['api_token'] = str_random(60);
+       $router  = Router::create($data);
+
+       $success['token']    =  $router->createToken('MyApp')->accessToken;
+       $success['hostname'] =  $router->hostname;
+       $success['loopback'] =  $router->loopback;
+ 
+       return response()->json(['success'=>$success], $this->successStatus);
+ 
+   }
+
+   /**
+    * update router api - based on IP
+    *
+    * @return \Illuminate\Http\Response
+    */
+ 
+    public function update(Request $request)
+    {
+ 
+        $validator = Validator::make($request->all(), [
+  
+             'sap_id'    => 'required', 
+             'hostname'  => 'required|unique',
+             'type'      => 'required',
+             'loopback'  => 'required|unique',
+             'mac_address' => 'required',
+  
+        ]);
+   
+        if ($validator->fails()) {
+  
+            return response()->json(['error'=>$validator->errors()], 401);            
+  
+        }
+  
+        $data    = $request->all();
+        $router  = Router::where('id',$ip_address)->update($data);
+ 
+        // $success['token']    =  $router->createToken('MyApp')->accessToken;
+        // $success['hostname'] =  $router->hostname;
+        // $success['loopback'] =  $router->loopback;
+  
+        return response()->json(['success'=>$router], $this->successStatus);
+  
+    }
+ 
+
+   /**
+    * delete router api - based on IP - softdelete
+    *
+    * @return \Illuminate\Http\Response
+    */
+
+    public function delete( Router $router )
+    {
+        return 'komal';
+    }
+ 
+   /**
+    * details api 
+    *
+    * @return \Illuminate\Http\Response
+    */
+ 
+   public function getDetails()
+   {# code...
+       $user = Auth::user();
+       return response()->json(['success' => $user], $this->successStatus);
+   }
+ 
+}
--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('login', 'Api\PassportController@login');
- 
 Route::post('register', 'Api\PassportController@register');
- 
 Route::group(['middleware' => 'auth:api'], function(){
- 
-Route::post('get-details', 'Api\PassportController@getDetails');
- 
+    Route::post('get-details', 'Api\PassportController@getDetails');
+});
+
+Route::group(['as' => 'router::', 'prefix' => 'router'], function () {
+
+    Route::post('create', 'Api\CiscoRouterController@create');
+    Route::post('update', 'Api\CiscoRouterController@update');
+    Route::post('delete', 'Api\CiscoRouterController@delete');
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::post('get-details', 'Api\CiscoRouterController@getDetails');
+    });
+
 });
